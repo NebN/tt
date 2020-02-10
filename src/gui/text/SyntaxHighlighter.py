aee44831c6a1438c871da2b8d4780f6a16dc6856
NebN <danielben94@gmail.com>
2020-02-10 08:15:56 +0100

    added syntax highlighting for comments and flags

--- src/gui/text/SyntaxHighlighter.py
+++ src/gui/text/SyntaxHighlighter.py
@@ -10,17 +10,30 @@ class SyntaxHighlighter(QSyntaxHighlighter):
         keyword_format.setFontWeight(QFont.Bold)
         string_format = QTextCharFormat()
         string_format.setForeground(QColor('#DAF7A6'))
+        flags_format = QTextCharFormat()
+        flags_format.setForeground(QColor('#FFC300'))
+        comment_format = QTextCharFormat()
+        comment_color = QColor('#FDF2E9')
+        comment_color.setAlpha(80)
+        comment_format.setForeground(comment_color)
 
         keywords = ['replace', 'with', 'sort', 'grep', 'distinct', 'join', 'split']
 
         self.rules = []
 
         # strings
-        self.rules.append(('\".*\"', string_format))
+        self.rules.append((r'''"[^"\\]*(\\.[^"\\]*)*"''', string_format))
+
+        # comments
+        self.rules.append((r'''--[^\n]*''', comment_format))
+        self.rules.append((r'''//[^\n]*''', comment_format))
+
+        # flags
+        self.rules.append((r'-\w+', flags_format))
 
         # keywords
         for word in keywords:
-            regex = QRegExp(f'\\b{word}\\b')
+            regex = QRegExp(fr'\b{word}\b')
             self.rules.append((regex, keyword_format))
 
     def highlightBlock(self, text):
